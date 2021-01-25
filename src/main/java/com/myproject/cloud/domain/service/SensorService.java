820b1d61bdeda1fb5b19a3f6cdd49b3ffb14fa64
quang.hoang <hoangvinhquang993@gmail.com>
2021-01-25 19:09:30 +0700

    not complete

--- src/main/java/com/myproject/cloud/domain/service/SensorService.java
+++ src/main/java/com/myproject/cloud/domain/service/SensorService.java
@@ -1,4 +1,9 @@
 package com.myproject.cloud.domain.service;
 
+import com.myproject.cloud.domain.model.SensorModel;
+
+import java.util.List;
+
 public interface SensorService {
+    List<SensorModel> getAllSensor();
 }
--- src/main/java/com/myproject/cloud/domain/service/impl/SensorServiceImpl.java
+++ src/main/java/com/myproject/cloud/domain/service/impl/SensorServiceImpl.java
@@ -1,8 +1,31 @@
 package com.myproject.cloud.domain.service.impl;
 
+import com.myproject.cloud.domain.model.SensorModel;
+import com.myproject.cloud.domain.repository.SensorRepository;
 import com.myproject.cloud.domain.service.SensorService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
+@RequiredArgsConstructor
 public class SensorServiceImpl implements SensorService {
+
+    private final SensorRepository sensorRepository;
+
+    @Override
+    public List<SensorModel> getAllSensor() {
+        return sensorRepository.findAll().stream().map(
+                sensorEntity -> {
+                    SensorModel sensorModel = new SensorModel();
+                    sensorModel.setSensorId(sensorEntity.getSensorId());
+                    sensorModel.setSensorName(sensorEntity.getSensorName());
+                    sensorModel.setNote(sensorEntity.getNote());
+                    sensorModel.setUserId(sensorEntity.getUserId());
+                    return sensorModel;
+                }
+        ).collect(Collectors.toList());
+    }
 }
